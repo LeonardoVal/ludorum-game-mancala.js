83cea2376cd4b10fa10be50d3d7687e55b59cf2f
Leonardo Val <leonardo.val@creatartis.com>
2017-08-13 17:35:27 -0300

    First build

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+﻿/** Gruntfile for [ludorum-game-mancala.js](http://github.com/LeonardoVal/ludorum-game-mancala.js).
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+
+	require('creatartis-grunt').config(grunt, {
+		sourceNames: ['__prologue__', 'Mancala', '__epilogue__'],
+		deps: [
+			{ name: 'creatartis-base', id: 'base',
+				path: 'node_modules/creatartis-base/build/creatartis-base.min.js' },
+			{ name: 'sermat', id: 'Sermat',
+				path: 'node_modules/sermat/build/sermat-umd.js' },
+			{ name: 'ludorum', id: 'ludorum',
+				path: 'node_modules/ludorum/build/ludorum.min.js' }
+		]
+	});
+
+	grunt.registerTask('default', ['build']);
+};
